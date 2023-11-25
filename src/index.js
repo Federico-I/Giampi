69361d58ef90ab85e1ecf1506a26a6714dd10ef6
Federico-I <threxos.48@gmail.com>
2023-11-25 01:17:27 +0000

    check-function-missing-data

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+
+function App() {
+  return (<h1>La Pizza</h1>)
+}
+
+function Pizza() {
+  return (<h1>Pizza 1</h1>)
+}
+
+// React 18
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
